0ba375d1c326e6c840165ec9319f224a21bbec9e
panx <xipan@bigvisiontech.com>
2020-04-28 16:24:35 +0800

    命名修改

--- pim-server/src/main/java/com/github/pim/server/core/ChannelSupervise.java
+++ pim-server/src/main/java/com/github/pim/server/core/ChannelSupervise.java
@@ -20,45 +20,46 @@ import java.util.concurrent.ConcurrentMap;
  * @since 1.0
  */
 public class ChannelSupervise {
-    private static ChannelGroup GlobalGroup = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
-    private static ConcurrentMap<String, ChannelId> ChannelMap = new ConcurrentHashMap<>();
+    private static ChannelGroup GLOBAL_GROUP = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+    private static ConcurrentMap<String, ChannelId> CHANNEL_MAP = new ConcurrentHashMap<>();
+
 
     public synchronized static void addChannel(String apiToken, Channel channel) {
-        GlobalGroup.add(channel);
+        GLOBAL_GROUP.add(channel);
         if (null != apiToken) {
-            ChannelMap.put(apiToken, channel.id());
+            CHANNEL_MAP.put(apiToken, channel.id());
         }
     }
 
     public synchronized static void updateChannel(String apiToken, Channel channel) {
-        Channel chan = GlobalGroup.find(channel.id());
+        Channel chan = GLOBAL_GROUP.find(channel.id());
         if (null == chan) {
             addChannel(apiToken, channel);
         } else {
-            ChannelMap.put(apiToken, channel.id());
+            CHANNEL_MAP.put(apiToken, channel.id());
         }
     }
 
     public synchronized static void removeChannel(Channel channel) {
-        GlobalGroup.remove(channel);
-        Collection<ChannelId> values = ChannelMap.values();
+        GLOBAL_GROUP.remove(channel);
+        Collection<ChannelId> values = CHANNEL_MAP.values();
         values.remove(channel.id());
     }
 
     public synchronized static Channel findChannel(String apiToken) {
-        ChannelId chanId = ChannelMap.get(apiToken);
+        ChannelId chanId = CHANNEL_MAP.get(apiToken);
         if (null == chanId) {
             return null;
         }
 
-        return GlobalGroup.find(ChannelMap.get(apiToken));
+        return GLOBAL_GROUP.find(CHANNEL_MAP.get(apiToken));
     }
 
     public static void sendToAll(TextWebSocketFrame tws) {
-        GlobalGroup.writeAndFlush(tws);
+        GLOBAL_GROUP.writeAndFlush(tws);
     }
 
     public static void sendToSimple(String apiToken, TextWebSocketFrame tws) {
-        GlobalGroup.find(ChannelMap.get(apiToken)).writeAndFlush(tws);
+        GLOBAL_GROUP.find(CHANNEL_MAP.get(apiToken)).writeAndFlush(tws);
     }
 }
